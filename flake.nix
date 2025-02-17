c9024bd96d62f7503237648629334b921ac5cd7c
AMS21 <AMS21.github@gmail.com>
2025-02-17 22:29:21 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "rustcc";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+          ];
+        };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default.override {
+            extensions = ["rust-src"];
+          }}/lib/rustlib/src/rust/library";
+
+          buildInputs = with pkgs; [
+            # Tooling
+            (pkgs.rust-bin.stable.latest.default.override {
+              extensions = ["rust-src" "cargo" "rustc"];
+            })
+            mold
+            sccache
+          ];
+
+          RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
+          RUSTFLAGS = "-Clink-arg=-fuse-ld=mold";
+        };
+      }
+    );
+}
