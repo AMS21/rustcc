1485a7d69c7bfbd7eb39eff0c35b222b74621bff
AMS21 <ams21.github@gmail.com>
2025-09-11 17:29:51 +0200

    feat: Implement some basic tests for TokenKind

--- rustcc/src/token.rs
+++ rustcc/src/token.rs
@@ -72,7 +72,16 @@ impl TokenKind {
     pub const fn is_binary_operator(&self) -> bool {
         matches!(
             self,
-            Self::Plus | Self::Minus | Self::Star | Self::Slash | Self::Percent
+            Self::Plus
+                | Self::Minus
+                | Self::Star
+                | Self::Slash
+                | Self::Percent
+                | Self::Ampersand
+                | Self::Caret
+                | Self::Pipe
+                | Self::GreaterThanGreaterThan
+                | Self::LessThanLessThan
         )
     }
 
@@ -461,5 +470,340 @@ impl<'a> Token<'a> {
             self.range.end.column,
             self.source_text().unwrap_or_default()
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ast::{BinaryOperator, UnaryOperator};
+
+    fn all_symbol_tokens() -> Vec<TokenKind> {
+        vec![
+            TokenKind::LeftParenthesis,
+            TokenKind::RightParenthesis,
+            TokenKind::LeftBrace,
+            TokenKind::RightBrace,
+            TokenKind::Semicolon,
+            TokenKind::Slash,
+            TokenKind::Tilde,
+            TokenKind::Minus,
+            TokenKind::MinusMinus,
+            TokenKind::Plus,
+            TokenKind::PlusPlus,
+            TokenKind::Star,
+            TokenKind::Percent,
+            TokenKind::Ampersand,
+            TokenKind::Caret,
+            TokenKind::Pipe,
+            TokenKind::LessThan,
+            TokenKind::LessThanEqual,
+            TokenKind::LessThanLessThan,
+            TokenKind::GreaterThan,
+            TokenKind::GreaterThanEqual,
+            TokenKind::GreaterThanGreaterThan,
+        ]
+    }
+
+    #[test]
+    fn from_identifier_maps_keywords_and_idents() {
+        assert_eq!(TokenKind::from_identifier("int"), TokenKind::KeywordInt);
+        assert_eq!(
+            TokenKind::from_identifier("return"),
+            TokenKind::KeywordReturn
+        );
+        assert_eq!(TokenKind::from_identifier("void"), TokenKind::KeywordVoid);
+
+        // Non-keywords remain identifiers (case-sensitive and arbitrary names)
+        assert_eq!(
+            TokenKind::from_identifier("Int"),
+            TokenKind::Identifier(String::from("Int"))
+        );
+        assert_eq!(
+            TokenKind::from_identifier("foo_bar123"),
+            TokenKind::Identifier(String::from("foo_bar123"))
+        );
+    }
+
+    #[test]
+    fn is_keyword_only_for_keywords() {
+        assert!(
+            TokenKind::KeywordInt.is_keyword(),
+            "KeywordInt should be keyword"
+        );
+        assert!(
+            TokenKind::KeywordReturn.is_keyword(),
+            "KeywordReturn should be keyword"
+        );
+        assert!(
+            TokenKind::KeywordVoid.is_keyword(),
+            "KeywordVoid should be keyword"
+        );
+
+        // Non-keywords
+        assert!(
+            !TokenKind::Identifier("x".into()).is_keyword(),
+            "Identifier should not be keyword"
+        );
+        assert!(
+            !TokenKind::IntegerLiteral(0).is_keyword(),
+            "IntegerLiteral should not be keyword"
+        );
+        for tk in all_symbol_tokens() {
+            assert!(
+                !tk.is_keyword(),
+                "symbol token unexpectedly marked as keyword: {tk:?}"
+            );
+        }
+    }
+
+    #[test]
+    fn is_identifier_only_for_identifier_variant() {
+        assert!(
+            TokenKind::Identifier("x".into()).is_identifier(),
+            "Identifier should be identifier"
+        );
+        // Negatives
+        assert!(
+            !TokenKind::KeywordInt.is_identifier(),
+            "KeywordInt should not be identifier"
+        );
+        assert!(
+            !TokenKind::KeywordReturn.is_identifier(),
+            "KeywordReturn should not be identifier"
+        );
+        assert!(
+            !TokenKind::KeywordVoid.is_identifier(),
+            "KeywordVoid should not be identifier"
+        );
+        assert!(
+            !TokenKind::IntegerLiteral(42).is_identifier(),
+            "IntegerLiteral should not be identifier"
+        );
+        for tk in all_symbol_tokens() {
+            assert!(
+                !tk.is_identifier(),
+                "symbol token unexpectedly marked as identifier: {tk:?}"
+            );
+        }
+    }
+
+    #[test]
+    fn is_binary_operator_flags_only_arithmetic_operators() {
+        for tk in [
+            TokenKind::Plus,
+            TokenKind::Minus,
+            TokenKind::Star,
+            TokenKind::Slash,
+            TokenKind::Percent,
+            TokenKind::Ampersand,
+            TokenKind::Caret,
+            TokenKind::Pipe,
+            TokenKind::LessThanLessThan,
+            TokenKind::GreaterThanGreaterThan,
+        ] {
+            assert!(tk.is_binary_operator(), "{tk:?} should be binary op flag");
+        }
+
+        // Others are not flagged
+        for tk in [
+            TokenKind::PlusPlus,
+            TokenKind::MinusMinus,
+            TokenKind::Tilde,
+            TokenKind::LeftParenthesis,
+            TokenKind::RightParenthesis,
+            TokenKind::LeftBrace,
+            TokenKind::RightBrace,
+            TokenKind::Semicolon,
+            TokenKind::LessThan,
+            TokenKind::LessThanEqual,
+            TokenKind::GreaterThan,
+            TokenKind::GreaterThanEqual,
+        ] {
+            assert!(
+                !tk.is_binary_operator(),
+                "{tk:?} should NOT be binary op flag"
+            );
+        }
+        assert!(
+            !TokenKind::IntegerLiteral(1).is_binary_operator(),
+            "IntegerLiteral should not be binary operator"
+        );
+        assert!(
+            !TokenKind::Identifier("x".into()).is_binary_operator(),
+            "Identifier should not be binary operator"
+        );
+        assert!(
+            !TokenKind::KeywordInt.is_binary_operator(),
+            "KeywordInt should not be binary operator"
+        );
+    }
+
+    #[test]
+    fn binary_operator_mapping_matches_expected() {
+        // Arithmetic
+        assert_eq!(TokenKind::Plus.binary_operator(), Some(BinaryOperator::Add));
+        assert_eq!(
+            TokenKind::Minus.binary_operator(),
+            Some(BinaryOperator::Subtract)
+        );
+        assert_eq!(
+            TokenKind::Star.binary_operator(),
+            Some(BinaryOperator::Multiply)
+        );
+        assert_eq!(
+            TokenKind::Slash.binary_operator(),
+            Some(BinaryOperator::Divide)
+        );
+        assert_eq!(
+            TokenKind::Percent.binary_operator(),
+            Some(BinaryOperator::Remainder)
+        );
+
+        // Bitwise and shifts
+        assert_eq!(
+            TokenKind::Ampersand.binary_operator(),
+            Some(BinaryOperator::BitwiseAnd)
+        );
+        assert_eq!(
+            TokenKind::Caret.binary_operator(),
+            Some(BinaryOperator::BitwiseXor)
+        );
+        assert_eq!(
+            TokenKind::Pipe.binary_operator(),
+            Some(BinaryOperator::BitwiseOr)
+        );
+        assert_eq!(
+            TokenKind::LessThanLessThan.binary_operator(),
+            Some(BinaryOperator::BitwiseLeftShift)
+        );
+        assert_eq!(
+            TokenKind::GreaterThanGreaterThan.binary_operator(),
+            Some(BinaryOperator::BitwiseRightShift)
+        );
+
+        // Non-operators map to None
+        for tk in [
+            TokenKind::LeftParenthesis,
+            TokenKind::RightParenthesis,
+            TokenKind::LeftBrace,
+            TokenKind::RightBrace,
+            TokenKind::Semicolon,
+            TokenKind::PlusPlus,
+            TokenKind::MinusMinus,
+            TokenKind::Tilde,
+            TokenKind::LessThan,
+            TokenKind::LessThanEqual,
+            TokenKind::GreaterThan,
+            TokenKind::GreaterThanEqual,
+            TokenKind::KeywordInt,
+            TokenKind::KeywordReturn,
+            TokenKind::KeywordVoid,
+            TokenKind::Identifier("x".into()),
+            TokenKind::IntegerLiteral(0),
+        ] {
+            assert_eq!(tk.binary_operator(), None, "unexpected mapping for {tk:?}");
+        }
+    }
+
+    #[test]
+    fn is_unary_operator_flags_plus_minus_tilde() {
+        assert!(
+            TokenKind::Plus.is_unary_operator(),
+            "+ should be unary operator"
+        );
+        assert!(
+            TokenKind::Minus.is_unary_operator(),
+            "- should be unary operator"
+        );
+        assert!(
+            TokenKind::Tilde.is_unary_operator(),
+            "~ should be unary operator"
+        );
+
+        for tk in [
+            TokenKind::Star,
+            TokenKind::Slash,
+            TokenKind::Percent,
+            TokenKind::Ampersand,
+            TokenKind::Caret,
+            TokenKind::Pipe,
+            TokenKind::LessThan,
+            TokenKind::LessThanEqual,
+            TokenKind::LessThanLessThan,
+            TokenKind::GreaterThan,
+            TokenKind::GreaterThanEqual,
+            TokenKind::GreaterThanGreaterThan,
+            TokenKind::PlusPlus,
+            TokenKind::MinusMinus,
+            TokenKind::LeftParenthesis,
+            TokenKind::RightParenthesis,
+            TokenKind::LeftBrace,
+            TokenKind::RightBrace,
+            TokenKind::Semicolon,
+        ] {
+            assert!(
+                !tk.is_unary_operator(),
+                "{tk:?} should NOT be unary operator"
+            );
+        }
+        assert!(
+            !TokenKind::IntegerLiteral(1).is_unary_operator(),
+            "IntegerLiteral should not be unary operator"
+        );
+        assert!(
+            !TokenKind::Identifier("x".into()).is_unary_operator(),
+            "Identifier should not be unary operator"
+        );
+        assert!(
+            !TokenKind::KeywordInt.is_unary_operator(),
+            "KeywordInt should not be unary operator"
+        );
+    }
+
+    #[test]
+    fn unary_operator_mapping_matches_expected() {
+        assert_eq!(
+            TokenKind::Minus.unary_operator(),
+            Some(UnaryOperator::Negate)
+        );
+        assert_eq!(
+            TokenKind::Tilde.unary_operator(),
+            Some(UnaryOperator::Complement)
+        );
+        assert_eq!(
+            TokenKind::Plus.unary_operator(),
+            Some(UnaryOperator::Positive)
+        );
+
+        // Others map to None
+        for tk in [
+            TokenKind::Star,
+            TokenKind::Slash,
+            TokenKind::Percent,
+            TokenKind::Ampersand,
+            TokenKind::Caret,
+            TokenKind::Pipe,
+            TokenKind::LessThan,
+            TokenKind::LessThanEqual,
+            TokenKind::LessThanLessThan,
+            TokenKind::GreaterThan,
+            TokenKind::GreaterThanEqual,
+            TokenKind::GreaterThanGreaterThan,
+            TokenKind::PlusPlus,
+            TokenKind::MinusMinus,
+            TokenKind::LeftParenthesis,
+            TokenKind::RightParenthesis,
+            TokenKind::LeftBrace,
+            TokenKind::RightBrace,
+            TokenKind::Semicolon,
+            TokenKind::KeywordInt,
+            TokenKind::KeywordReturn,
+            TokenKind::KeywordVoid,
+            TokenKind::Identifier("x".into()),
+            TokenKind::IntegerLiteral(0),
+        ] {
+            assert_eq!(tk.unary_operator(), None, "unexpected mapping for {tk:?}");
+        }
     }
 }
